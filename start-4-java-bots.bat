4e4cefd3f9e94f56fd1cd2a7ab1520cf0b01a7da
hanby <boyouhan@gmail.com>
2022-09-28 02:08:18 -0500

    removed debug from start bots bat

--- start-4-java-bots.bat
+++ start-4-java-bots.bat
@@ -1,4 +1,4 @@
-start cmd /k java -jar -Ddebug=true .\build\libs\Java-Starterpack-1.0-SNAPSHOT.jar 0
-start cmd /k java -jar -Ddebug=true .\build\libs\Java-Starterpack-1.0-SNAPSHOT.jar 1
-start cmd /k java -jar  .\build\libs\Java-Starterpack-1.0-SNAPSHOT.jar 2
-start cmd /k java -jar -Ddebug=true .\build\libs\Java-Starterpack-1.0-SNAPSHOT.jar 3
+start cmd /k java -jar .\build\libs\Java-Starterpack-1.0-SNAPSHOT.jar 0
+start cmd /k java -jar .\build\libs\Java-Starterpack-1.0-SNAPSHOT.jar 1
+start cmd /k java -jar .\build\libs\Java-Starterpack-1.0-SNAPSHOT.jar 2
+start cmd /k java -jar .\build\libs\Java-Starterpack-1.0-SNAPSHOT.jar 3
