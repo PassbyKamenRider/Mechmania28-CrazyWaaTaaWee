722e074d724a79570157512207d4f3a785cbd13e
Han <boyouh2@illinois.edu>
2022-10-01 14:46:59 -0500

    updated characterclass stats

--- src/main/java/starterpack/strategy/StrategyConfig.java
+++ src/main/java/starterpack/strategy/StrategyConfig.java
@@ -8,8 +8,6 @@ public class StrategyConfig {
      * @return A Strategy object.
      */
     public static Strategy getStrategy(int playerIndex) {
-//        if (playerIndex == 0) return new OnlyBuyAndUseShieldStrategy();
-//        else return new OnlyGoto00AndAttackStrategy();
         return new RandomStrategy();
     }
 }
